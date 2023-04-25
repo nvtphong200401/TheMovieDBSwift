c10c7ac3db23bc3c5270f21839a40733a4140cac
nvtphong-Executionlab <phong.nguyen@executionlab.asia>
2023-04-25 09:50:02 +0700

    Initial Commit

---
+++ TheMovieDBSwift/TheMovieDBSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieDBSwiftApp.swift
+//  TheMovieDBSwift
+//
+//  Created by exe-macm1 on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDBSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
